52a1528aa3e974b3d4629c3fcac4822d54d41dd6
KaeseKuchenDEV <hendrikloewe3@gmail.com>
2021-04-11 12:06:36 +0200

    wipe old website

--- src/app/models/clanAuthority.ts
+++
@@ -1,9 +0,0 @@
-export class ClanAuthority {
-    userName: string;
-    isFounder: boolean;
-
-    constructor(userName: string, isFounder: boolean) {
-        this.userName = userName;
-        this.isFounder = isFounder;
-    }
-}
